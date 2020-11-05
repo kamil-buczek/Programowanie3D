82e84051ddbc39a2b5817e80c51241bb9a3ce252
Kamil Buczek <kamil.andrzej.buczek@gmail.com>
2020-11-05 17:59:29 +0100

    Zadanie3 - ready to check

--- src/Exercises/Indices/app.cpp
+++ src/Exercises/Indices/app.cpp
@@ -23,8 +23,6 @@ void SimpleShapeApplication::init() {
         std::cerr << std::string(PROJECT_DIR) + "/shaders/base_fs.glsl" << " shader files" << std::endl;
     }
 
-
-
     std::vector<GLfloat> domek = {
             0.0f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f,
             -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
@@ -32,10 +30,21 @@ void SimpleShapeApplication::init() {
             -0.5f, 0.0f, 0.0f, 0.5f, 0.0f, 1.0f,
             0.5f, 0.0f, 0.0f, 0.5f, 0.0f, 1.0f,
             -0.5f, -1.0f, 0.0f, 0.5f, 0.0f, 1.0f,
-            -0.5f, -1.0f, 0.0f, 0.5f, 0.0f, 1.0f,
+            //-0.5f, -1.0f, 0.0f, 0.5f, 0.0f, 1.0f,
             0.5f, -1.0f, 0.0f, 0.5f, 0.0f, 1.0f,
-            0.5, 0.0f, 0.0f, 0.5f, 0.0f, 1.0f
+            //0.5f, 0.0f, 0.0f, 0.5f, 0.0f, 1.0f
     };
+
+    std::vector<GLushort> indices = {
+            0,1,2,3,4,5,6,5,4 // wypisujemy tyle elementów ile mamy wierzchołków
+    };
+    GLuint idx_buffer_handle;
+    glGenBuffers(1,&idx_buffer_handle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(),
+                 GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 
     GLuint v_buffer_handle;
     glGenBuffers(1, &v_buffer_handle);
@@ -47,6 +56,7 @@ void SimpleShapeApplication::init() {
     glGenVertexArrays(1, &vao_);
     glBindVertexArray(vao_);
     glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+
     glEnableVertexAttribArray(0);
     //Czyli współrzędne wierzchołka trójkąta pobierz z 3 wartości
     // zaczynając od 0 (reinterpret_cast<GLvoid *>(0)) i przeskakuj o 6 następnych (6 * sizeof(GLfloat))
@@ -56,6 +66,9 @@ void SimpleShapeApplication::init() {
     // od 3 (reinterpret_cast<GLvoid *>(3*sizeof(GLfloat))) i przeskakuj o 6 do następnych (6 * sizeof(GLfloat))
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3*sizeof(GLfloat)));
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle); //dodane w zadaniu 3
+
     glBindVertexArray(0);
 
 
@@ -70,6 +83,7 @@ void SimpleShapeApplication::init() {
 
 void SimpleShapeApplication::frame() {
     glBindVertexArray(vao_);
-    glDrawArrays(GL_TRIANGLES, 0, 9);
+    //glDrawArrays(GL_TRIANGLES, 0, 9);
+    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_SHORT, reinterpret_cast<GLvoid*>(0));
     glBindVertexArray(0);
 }
